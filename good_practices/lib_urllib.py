8d01378f65db61b1a21c7fa7e31d0db2d5e2ad4c
Etienne Kornobis <ekornobis@gmail.com>
2016-08-10 17:59:31 +0200

    Adding stub to use beautifulsoup

--- good_practices/lib_urllib.py
+++ good_practices/lib_urllib.py
@@ -1,4 +1,15 @@
 import urllib.request
+# this is for python3:
+from bs4 import BeautifulSoup
 
 # HTTP File Download
-urllib.request.urlretrieve('https://framasoft.org/accueil/img/pingouinVolantRefait.png','pic.png')
+urllib.request.urlretrieve('https://framasoft.org/accueil/img/pingouinVolantRefait.png',
+                           'pic.png')
+
+# Retrieve an HTML
+with urllib.request.urlopen('http://python.org/') as response:
+    html = response.read()
+
+# Find a div (TO IMPROVE, at least the example)
+    soup = BeautifulSoup(html, "lxml")
+#    print(soup.body.find('div', attrs={'id':'oldie-warning'}).text)
