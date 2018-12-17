a038234b30cc534d82ea79d9a19971c6136016a6
Etienne Kornobis <ekornobis@gmail.com>
2018-12-17 08:27:33 +0100

    Adding comment for multiprocessing

--- good_practices/lib_multiprocessing.py
+++ good_practices/lib_multiprocessing.py
@@ -1,13 +1,24 @@
 from multiprocessing import Pool
-from functools import partial # For multiple arguments
+from functools import partial  # For multiple arguments
 
 
 def f(x):
-    return x*x
+    return x * x
+
 
 # USING of the partial trick to pass other parameters
 
-if __name__ == '__main__':
+
+# To use a class method on a list of objects
+from operator import methodcaller
+
+map(methodcaller("methodname"), object_list)
+
+# Or pass by lambda:
+
+map(lambda obj: obj.method(), objectlist)
+
+
+if __name__ == "__main__":
     p = Pool(5)
     print(p.map(f, [1, 2, 3]))
-
